76b7c81728b2458ea247d31fa1d90ba1dbf9715a
nate-seligson <146794258+nate-seligson@users.noreply.github.com>
2024-04-01 17:52:26 -0400

    nice

--- tasks.js
+++ tasks.js
@@ -3,6 +3,18 @@ export class Task{
         this.object = document.createElement("div");
         this.object.innerHTML = title
         this.object.style.backgroundColor = color
+        this.object.className = "task"
         document.body.appendChild(this.object)
+
+        this.checkbox = document.createElement("input")
+        this.checkbox.type = "checkbox"
+        this.checkbox.className = "cbox"
+        this.object.appendChild(this.checkbox)
     }
+    StrikeThrough(){
+        this.line = document.createElement("div");
+    }
+}
+export class TaskList{
+
 }
