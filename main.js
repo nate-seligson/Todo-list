7b329f361e0d75e0d81142ba81c23d92e3e83843
nate-seligson <146794258+nate-seligson@users.noreply.github.com>
2024-04-22 15:00:00 -0400

    Color changes and lots of cool stuff

--- main.js
+++ main.js
@@ -1,5 +1,36 @@
 import { Task } from "./tasks.js";
+const inputtask = document.getElementById("inputtask");
+const newtask = document.getElementById("newtask");
+const colorinput = document.getElementById("colorinput");
 function CreateTask(input = "", color = "red"){
     new Task(input, color)
 }
-document.getElementById("newtask").addEventListener("click", function(){CreateTask(document.getElementById("inputtask").value, document.getElementById("colorinput").value); document.getElementById("inputtask").value = ""})
+function MakeTask(){
+    CreateTask(
+        inputtask.value, 
+        colorinput.value
+    );
+    inputtask.value = ""
+}
+newtask.addEventListener("click", function(){MakeTask();})
+inputtask.addEventListener("keydown", function(event){
+    if(event.key == "Enter"){
+        MakeTask()
+    }
+})
+//detect keys
+document.addEventListener("keydown", function(event){
+    switch(event.key){
+        case "Control":
+        case "Meta":
+            if(document.activeElement != inputtask){
+                inputtask.focus();
+            }
+            break;
+        case "ArrowRight":
+            colorinput.style.transform = "translate(700%)";
+            colorinput.style.opacity = "100%";
+            colorinput.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+            break;
+    }
+})
--- tasks.js
+++ tasks.js
@@ -9,19 +9,23 @@ export class Task{
         document.getElementById("tasks").appendChild(this.object)
 
         this.line = document.createElement("div");
-        this.line.style.width = this.object.style.width
         this.line.className = "line"
+        this.line.style.top += ((this.object.clientHeight/2)/window.innerWidth * 100).toString() + "vw";
         this.object.appendChild(this.line)
         this.object.addEventListener("click", this.StrikeThrough.bind(this))
     }
     StrikeThrough(){
         if(!this.checked){
-            this.line.style.display = "block"
+            this.line.style.borderWidth = (this.object.clientHeight / 175).toString() + "vw";
+            this.line.style.width="110%";
+            this.line.style.transition = "0.5s ease"
             this.checked = true;
         }
         else{
-            this.line.style.display = "none"
+            this.line.style.borderWidth = "0vw";
+            this.line.style.width="0%";
             this.checked = false;
+            this.line.style.transition = "0.1s ease"
         }
     }
 }
