bdefc2c6b5cb10099f9d5adf220faa5abeb79e1f
nate-seligson <146794258+nate-seligson@users.noreply.github.com>
2024-04-22 18:32:54 -0400

    Final?

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ import { Task } from "./tasks.js";
 const inputtask = document.getElementById("inputtask");
 const newtask = document.getElementById("newtask");
 const colorinput = document.getElementById("colorinput");
+const stuff = document.getElementById("stuff");
+
+//get local data
+JSON.parse("[" + localStorage.getItem("data") + "]").forEach(function(item){if(item!=null){new Task(item["title"], item["color"], item["checked"])}})
 function CreateTask(input = "", color = "red"){
     new Task(input, color)
+    localStorage.setItem("data", localStorage.getItem("data") + "," +JSON.stringify({"title": input, "color": color, "checked": false}))
 }
 function MakeTask(){
     CreateTask(
@@ -28,9 +33,10 @@ document.addEventListener("keydown", function(event){
             }
             break;
         case "ArrowRight":
-            colorinput.style.transform = "translate(700%)";
+            colorinput.style.left = "93%";
             colorinput.style.opacity = "100%";
             colorinput.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+            stuff.addEventListener("mouseout", function(){colorinput.removeAttribute("style")}, {once:true})
             break;
     }
 })
--- tasks.js
+++ tasks.js
@@ -1,6 +1,7 @@
 export class Task{
-    constructor(title, color){
-        this.checked = false;
+    constructor(title, color, checked = false){
+        this.checked = checked;
+        this.title = title;
 
         this.object = document.createElement("div");
         this.object.innerHTML = title
@@ -10,9 +11,12 @@ export class Task{
 
         this.line = document.createElement("div");
         this.line.className = "line"
-        this.line.style.top += ((this.object.clientHeight/2)/window.innerWidth * 100).toString() + "vw";
         this.object.appendChild(this.line)
         this.object.addEventListener("click", this.StrikeThrough.bind(this))
+        if(this.checked){
+            this.checked = false;
+            this.StrikeThrough()
+        }
     }
     StrikeThrough(){
         if(!this.checked){
@@ -20,13 +24,23 @@ export class Task{
             this.line.style.width="110%";
             this.line.style.transition = "0.5s ease"
             this.checked = true;
+            this.ChangeData("checked", true)
         }
         else{
             this.line.style.borderWidth = "0vw";
             this.line.style.width="0%";
             this.checked = false;
             this.line.style.transition = "0.1s ease"
+            this.ChangeData("checked", false)
         }
+    }
+    ChangeData(key,val){
+        let data = JSON.parse("[" + localStorage.getItem("data") + "]")
+        let toChange = data.findIndex((obj) => obj != null && obj["title"] == this.title)
+        let newData = data[toChange];
+        newData[key] = val;
+        data = JSON.stringify(data.with(toChange, newData)).slice(1,-1);
+        localStorage.setItem("data", data)
     }
 }
 export class TaskList{
